49145fdc75ece0ef75065ef7de5c288ce7918a6b
Robert <rfullerton07@gmail.com>
2021-02-07 20:43:35 -0800

    Mongoose Part 2

--- index.js
+++ index.js
@@ -12,9 +12,24 @@ connect.then(() => {
 
     console.log('Connected correctly to server');
 
-    const newCampsite = new Campsite({
+    Campsite.create({
         name: 'React Lake Campground',
         description: 'test'
+    })
+    .then(campsite => {
+        console.log(campsite);
+        return Campsite.find();
+    })
+    .then(campsites => {
+        console.log(campsites);
+        return Campsite.deleteMany();
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch(err => {
+        console.log(err);
+        mongoose.connection.close();
     });
 
     newCampsite.save()
